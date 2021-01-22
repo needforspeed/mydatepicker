6f8aff1236aacf064c7ebc1a4eaa1d1507a53b96
Yuncong Zhang <needforspeed@users.noreply.github.com>
2021-01-22 14:25:45 -0500

    Ivy compiler (#5)
    * initial commit
    
    * clean up
    
    * cleanup

---
+++ projects/my-date-picker/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of my-date-picker
+ */
+export * from './lib/my-date-picker';
+// export * from './lib/my-date-picker.service';
+// export * from './lib/my-date-picker.component';
+// export * from './lib/my-date-picker.module';
